22d4e77fc094b60cf1c33955ae30b894b14d54a1
Alisson Max <64440593+haazhelthorn@users.noreply.github.com>
2024-11-15 16:00:42 -0300

    api comunicando com o gemini

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,18 +1,38 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import {
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
 
-import { CreateNutritionController } from './controllers/CreateNutritionController'
+import { CreateNutritionController } from "./controllers/CreateNutritionController";
 
-export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-  
+export async function routes(
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions
+) {
   fastify.get("/teste", (request: FastifyRequest, reply: FastifyReply) => {
-    console.log("rota chamada");
-    
-    reply.send({ ok: true });
-  })
+    let responseText =
+      '```json\n{\n  "nome": "Alisson",\n  "sexo": "Masculino",\n  "idade": 31,\n  "altura": 180,\n  "peso": 85,\n  "objetivo": "Hipertrofia",\n  "refeicoes": [\n    {\n      "horario": "7:00",\n      "nome": "Cafe da manha",\n      "alimentos": [\n        "Aveia (50g)",\n        "Leite desnatado (200ml)",\n        "Banana (1 unidade)",\n        "Nozes (1 punhado)",\n        "Proteina de soro do leite (30g)"\n      ]\n    },\n    {\n      "horario": "10:00",\n      "nome": "Lanche da manha",\n      "alimentos": [\n        "Iogurte desnatado (1 unidade)",\n        "Frutas vermelhas (1 concha)"\n      ]\n    },\n    {\n      "horario": "13:00",\n      "nome": "Almoco",\n      "alimentos": [\n        "Arroz integral (1 concha)",\n        "Frango grelhado (150g)",\n        "Salada verde (1 concha)",\n        "Feijao (1 concha)"\n      ]\n    },\n    {\n      "horario": "16:00",\n      "nome": "Lanche da tarde",\n      "alimentos": [\n        "Batata doce (1 unidade media)",\n        "Queijo cottage (100g)"\n      ]\n    },\n    {\n      "horario": "19:00",\n      "nome": "Jantar",\n      "alimentos": [\n        "Carne vermelha magra (150g)",\n        "Batata doce (150g)",\n        "Brócolis (1 concha)"\n      ]\n    },\n    {\n      "horario": "21:00",\n      "nome": "Lanche antes de dormir",\n      "alimentos": [\n        "Caseina (30g)",\n        "Leite desnatado (100ml)"\n      ]\n    }\n  ],\n  "suplementos": [\n    "Proteina de soro do leite",\n    "Creatina",\n    "BCAA",\n    "Multivitaminico"\n  ]\n}\n```';
 
-  fastify.get("/create", async (request: FastifyRequest, reply: FastifyReply) => {
-    return new CreateNutritionController().handle(request, reply)
-  })
+    try {
+      let jsonString = responseText
+        .replace(/```\w*\n/g, "")
+        .replace(/\n```/g, "")
+        .trim();
 
+      let jsonObject = JSON.parse(jsonString);
 
+      return reply.send({ data: jsonObject });
+    } catch (err) {
+      console.log("err", err);
+    }
+  });
+
+  fastify.post(
+    "/create",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return new CreateNutritionController().handle(request, reply);
+    }
+  );
 }
--- backend/src/services/CreateNutritionService.ts
+++ backend/src/services/CreateNutritionService.ts
@@ -1,9 +1,53 @@
+import { DataProps } from "../controllers/CreateNutritionController";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 class CreateNutritionService {
-  async execute() {
+  async execute({
+    name,
+    weight,
+    height,
+    age,
+    gender,
+    objective,
+    level,
+  }: DataProps) {
+    try {
+      const apiKey = process.env.API_KEY!;
+      const genAI = new GoogleGenerativeAI(apiKey);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); // gemini-1.5-flash or gemini-1.5-pro
 
-    console.log("TESTE")
+      const response = await model.generateContent(
+        `Crie uma dieta completa para uma pessoa com nome: ${name}, ${age} anos, ${height}cm, ${weight}kg, 
+        ${gender} e com o foco e objetivo ${objective}, atualmente nível de atividade ${level} e ignore 
+        qualquer outro parametro que não seja passados, retorne um json com as respectivas propriedades, 
+        propriedade nome o nome da pessoa, propriedade sexo com sexo, propriedade idade, propriedade altura, 
+        propriedade peso, propriedade objetivo com o objetivo atual, propriedade refeições com uma array 
+        contendo dentro cada objeto sendo uma refeição da dieta e dentro de cada refeição a propriedade 
+        horário com horário da refeição, propriedade nome com nome e a propriedade alimentos com array 
+        contendo os alimentos dessa refeição e pode incluir uma propriedade com suplementos contendo array 
+        com sugestão de suplemento que é indicado para o sexo dessa pessoa e o objetivo dela e não retorne 
+        nenhuma observação além das passadas no prompt, retorne em json e nenhuma propriedade pode ter acento.`
+      );
 
-    return { message: "funcionou o service" }
+      console.log(JSON.stringify(response, null, 2));
+
+      if (response.response && response.response.candidates) {
+        const jsonText = response.response.candidates[0]?.content.parts[0]
+          .text as string;
+
+        let jsonString = jsonText
+          .replace(/```\w*\n/g, "")
+          .replace(/\n```/g, "")
+          .trim();
+
+        let jsonObject = JSON.parse(jsonString);
+
+        return { data: jsonObject };
+      }
+    } catch (err) {
+      console.error("Erro JSON: ", err);
+      throw new Error("Erro ao criar nutrição");
+    }
   }
 }
-  export { CreateNutritionService }
+export { CreateNutritionService };
