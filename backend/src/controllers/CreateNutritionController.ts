d7d9653599b09c607b7818519f47f4cb28b84fee
Alisson Max <64440593+haazhelthorn@users.noreply.github.com>
2024-11-15 14:36:48 -0300

    imp.: service

--- backend/src/controllers/CreateNutritionController.ts
+++ backend/src/controllers/CreateNutritionController.ts
@@ -1,10 +1,16 @@
 import {FastifyRequest, FastifyReply} from 'fastify';
+import {CreateNutritionService} from '../services/CreateNutritionService'
+
 
 class  CreateNutritionController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
     
+    const createNutrition = new CreateNutritionService();
+
+    const nutrition = await createNutrition.execute();
+
+    return reply.send(nutrition);
     
-    reply.send({ ok: true });
   }
 }
 
---
+++ backend/src/services/CreateNutritionService.ts
@@ -0,0 +1,9 @@
+class CreateNutritionService {
+  async execute() {
+
+    console.log("TESTE")
+
+    return { message: "funcionou o service" }
+  }
+}
+  export { CreateNutritionService }
